b187082e32a819e082245bbafe1269a29a86af14
Andy Shen <andyshenproper@gmail.com>
2021-11-14 11:17:11 -0800

    Add icons to manifest. Change create to update in background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.omnibox.onInputEntered.addListener((text) => {
   // Encode user input for special characters , / ? : @ & = + $ #
   let newURL = 'https://pypi.org/search/?q=' + encodeURIComponent(text);
-  chrome.tabs.create({ url: newURL });
+  chrome.tabs.update({ url: newURL });
 });
