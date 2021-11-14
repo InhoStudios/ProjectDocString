b07e077d712ce66452fd883752114057a9a3edef
Kai Chen <22359229+kx-chen@users.noreply.github.com>
2021-11-14 11:00:17 -0800

    background.js: Simple search for PyPi
    Also change manifest.json to V3

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.omnibox.onInputEntered.addListener((text) => {
+  // Encode user input for special characters , / ? : @ & = + $ #
+  let newURL = 'https://pypi.org/search/?q=' + encodeURIComponent(text);
+  chrome.tabs.create({ url: newURL });
+});
