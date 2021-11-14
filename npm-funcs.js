af94971ab5c3059b669a011955d80db886321959
Frank Hui <huifrank@live.com>
2021-11-14 12:16:44 -0800

    god has smiled upon us

--- npm-funcs.js
+++ npm-funcs.js
@@ -1,17 +1,18 @@
 async function searchNpm(searchTerm){
+    var obj;
     let url = 'https://stormy-tor-99017.herokuapp.com/https://registry.npmjs.org/' + searchTerm;
-    return fetch(url).then(function(resp){
-        return resp.json().homepage;
-    })
+    await fetch(url).then(res => res.json())
+        .then(data => obj = data)
+    return obj.homepage
 }
 
-function processJSON(obj){
-    return obj.homepage;
-}
+// function processJSON(obj){
+//     return obj.homepage;
+// }
 
 
-export default searchNpm;
-searchNpm("discord")
+// export default searchNpm;
+
 
 // https://registry.npmjs.org/
 // Docs: https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md
