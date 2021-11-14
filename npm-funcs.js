9cc78732822f59362d2b1d4a0909fdb95e5933b3
Frank Hui <huifrank@live.com>
2021-11-14 11:52:13 -0800

    Fix merge conflict in npm-funcs

--- npm-funcs.js
+++ npm-funcs.js
@@ -1,8 +1,17 @@
-function searchNpm(){
-
+async function searchNpm(searchTerm){
+    let url = 'https://stormy-tor-99017.herokuapp.com/https://registry.npmjs.org/' + searchTerm;
+    return fetch(url).then(function(resp){
+        return resp.json().homepage;
+    })
 }
 
+function processJSON(obj){
+    return obj.homepage;
+}
+
+
 export default searchNpm;
+searchNpm("discord")
 
 // https://registry.npmjs.org/
 // Docs: https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md
